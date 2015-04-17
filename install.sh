7cc45168df0c23b7083508443be14b3f83a15960
awaki75 <awaki.33s@gmail.com>
2015-04-17 02:05:01 +0000

    VIMの新規インストールをしない

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y curl git vim zsh
+sudo apt-get install -y curl git zsh
 
 if [ ! -d ~/.oh-my-zsh ]; then
     curl -s https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
