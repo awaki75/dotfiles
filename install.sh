0ea414979204707709baa3aff488fbb9f8939870
awaki75 <awaki.33s@gmail.com>
2016-05-03 00:44:53 +0900

    temp

--- install.sh
+++ install.sh
@@ -1,39 +1,55 @@
-sudo apt-get install -y git vim-nox zsh
+#!/bin/bash
 
-if [ ! -d ~/.oh-my-zsh ]; then
-    curl -s https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+while getopts m:n:s opt; do
+  case $opt in
+    m) mail="$OPTARG" ;;
+    n) name="$OPTARG" ;;
+    s) enabled_sudo=true ;;
+    *) exit 1 ;;
+  esac
+done
+
+: "sudo" && {
+  if [ $enabled_sudo ]; then
+    sudo apt-get -y install git vim-nox zsh
     sudo chsh -s /bin/zsh $USER
-fi
+  else
+    hash git vim zsh || exit 1
+  fi
+}
 
-if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
+: "install Oh My Zsh" && {
+  if [ ! -d ~/.oh-my-zsh ]; then
+    curl -s https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+  fi
+}
+
+: "install NeoBundle" && {
+  if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
     curl -s https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-fi
+  fi
+}
 
-mkdir -p ~/.oh-my-zsh/custom/themes
-url="https://raw.githubusercontent.com/awaki75/dotfiles/master"
-curl -s $url/.vimrc -o ~/.vimrc
-curl -s $url/.zshrc -o ~/.zshrc
-curl -s $url/awaki.zsh-theme -o ~/.oh-my-zsh/custom/themes/awaki.zsh-theme
+: "place dotfiles" && {
+  base="https://raw.githubusercontent.com/awaki75/dotfiles/master"
+  files=(
+    ".gitattributes"
+    ".oh-my-zsh/custom/themes/mine.zsh-theme"
+    ".vimrc"
+    ".zshrc"
+  )
+  for file in ${files[@]}; do
+    [ "$file" != "${file%/*}" ] && mkdir -p "~/${file%/*}"
+    curl -s "$base/$file" -o "~/$file"
+  done
+}
 
-# cd ~/.vim/bundle/jedi-vim
-# git submodule update --init
+: "configure Git" && {
+  [ -n "$mail" ] && git config --global user.email $mail
+  [ -n "$name" ] && git config --global user.name $name
+  git config --global color.ui auto
+  git config --global core.attributesfile ~/.gitattributes
+  git config --global core.editor vim
+}
 
-cat > ~/.gitattributes << EOF
-*.c diff=cpp
-*.h diff=cpp
-*.cpp diff=cpp
-*.hpp diff=cpp
-*.m diff=objc
-*.java diff=java
-*.html diff=html
-*.pl diff=perl
-*.pm diff=perl
-*.t diff=perl
-*.php diff=php
-*.py diff=python
-*.rb diff=ruby
-*.js diff=java
-EOF
-git config --global color.ui auto
-git config --global core.attributesfile ~/.gitattributes
-git config --global core.editor vim
+exit 0
