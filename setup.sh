48333fc35320c0ffce25e65f13d26910a532ef4e
awaki75 <awaki.33s@gmail.com>
2016-11-23 13:48:41 +0900

    出力

--- setup.sh
+++ setup.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
+echo "check commands"
 hash curl git zsh || exit
 
-[ -d ~/.zplug ] || curl -sL zplug.sh/installer | zsh
+echo "install zplug"
+if [ -d ~/.zplug ]; then
+    echo "  skipped"
+else
+    curl -sL zplug.sh/installer | zsh
+fi
 
-[ -d ~/dotfiles ] || git clone https://github.com/awaki75/dotfiles.git ~/dotfiles
+echo "download dotfiles"
+if [ -d ~/dotfiles ]; then
+    echo "  skipped"
+else
+    git clone https://github.com/awaki75/dotfiles.git ~/dotfiles
+fi
 
+echo "link"
 mkdir -p ~/.config/nvim
 cd ~
-
 for name in .config/nvim/init.vim .tmux.conf .zshrc; do
     ln -fs "~/dotfiles/src/$name" "$name"
 done
 
+echo "configure git"
 git config --global color.ui auto
 git config --global core.attributesfile ~/dotfiles/src/.gitattributes
 git config --global core.editor vim
 git config --global core.excludesfile ~/dotfiles/src/.gitignore
 git config --global push.default simple
+
+echo "completed"
