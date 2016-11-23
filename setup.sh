965fd68ebf96e0c0fe8784f34e1a6548185772e0
awaki75 <awaki.33s@gmail.com>
2016-11-23 12:58:13 +0900

    シンボリックリンクを貼るように変更

--- setup.sh
+++ setup.sh
@@ -4,14 +4,17 @@ hash curl git zsh || exit
 
 [ -d ~/.zplug ] || curl -sL zplug.sh/installer | zsh
 
-mkdir -p ~/.config/nvim
+[ -d ~/dotfiles ] || git clone https://github.com/awaki75/dotfiles.git ~/dotfiles
 
-for name in .config/nvim/init.vim .gitattributes .gitignore .tmux.conf .zshrc; do
-    curl -sLS "https://raw.githubusercontent.com/awaki75/dotfiles/master/src/$name" -o ~/$name
+mkdir -p ~/.config/nvim
+cd ~
+
+for name in .config/nvim/init.vim .tmux.conf .zshrc; do
+    ln -fs "~/dotfiles/src/$name" "$name"
 done
 
 git config --global color.ui auto
-git config --global core.attributesfile ~/.gitattributes
+git config --global core.attributesfile ~/dotfiles/src/.gitattributes
 git config --global core.editor vim
-git config --global core.excludesfile ~/.gitignore
+git config --global core.excludesfile ~/dotfiles/src/.gitignore
 git config --global push.default simple
